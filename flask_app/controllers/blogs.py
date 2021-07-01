47b9675ab647ba4bd1f15988e542c0b3d158198e
Jeremy Hatfield <jhatfieldkc@gmail.com>
2021-07-01 14:39:22 -0500

    completed blog pages

--- flask_app/controllers/blogs.py
+++ flask_app/controllers/blogs.py
@@ -1,6 +1,8 @@
+from werkzeug import datastructures
 from flask_app import app
 from flask import redirect, request, render_template, flash, session
 from flask_app.models import team, user, blog
+from flask_app.models.blog import Blog
 
 @app.route("/create_blog")
 def create_blog():
@@ -11,10 +13,17 @@ def create_blog():
 def save_blog():
     if  blog.Blog.validate_blog(request.form):
         data = {
-            'blog_title': request.form['blog_title'],
-            'blog_body': request.form['blog_body']
+            'title': request.form['blog_title'],
+            'body': request.form['blog_body'],
+            'author_id': session['user_id']
         }
         blog.Blog.create_blog(data)
         return redirect("/create_blog")
 
-    
+@app.route("/view_blog")
+def view_blog():
+
+    blogs = Blog.get_blog_by_id()
+    print(blogs)
+
+    return render_template("view_blog.html", blogs = blogs)
--- flask_app/models/blog.py
+++ flask_app/models/blog.py
@@ -1,18 +1,29 @@
 from flask_app.config.mysqlconnection import connectToMySQL
 from flask import request
 from flask import flash
+from flask_app.models.user import User
 
 class Blog:
     def __init__(self, data):
         self.id = data["id"]
         self.title = data["title"]
-        self.post = data["post"]
+        self.body = data["body"]
         self.author.id = data["author.id"]
+        self.creator = None
 
     @classmethod
     def create_blog(cls, data):
-        query = "INSERT INTO teams (name, captain_id) VALUES (%(name)s, %(captain_id)s);"
-        return connectToMySQL("pistol_pals_personal").query_db(query, data)
+        query = "INSERT INTO blog (title, body, author_id) VALUES (%(title)s, %(body)s, %(author_id)s);"
+        return connectToMySQL("pp").query_db(query, data)
+
+    @classmethod
+    def get_blog_by_id(cls):
+        query = 'SELECT * FROM blog ORDER BY created_at DESC;'
+
+        connection = connectToMySQL('pp')
+        results = connection.query_db(query)
+
+        return results
 
 
     @staticmethod
